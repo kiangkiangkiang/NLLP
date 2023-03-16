1a8ce6ac6432599f676a2b50fef2c2b4dcffe515
Bo Syue  Jiang <lukajiang1998@gmail.com>
2023-03-16 13:55:51 +0800

    test

--- labelstudio_backend/MyUIEPredictModel.py
+++ labelstudio_backend/MyUIEPredictModel.py
@@ -1,3 +1,8 @@
+import numpy as np
+import os
+import json
+from paddlenlp import Taskflow
+from label_studio_ml.model import LabelStudioMLBase
 def initial_parsed_label_config():
     result = {'label': {
               'type': 'Labels',
