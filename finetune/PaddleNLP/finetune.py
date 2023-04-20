77f4bebe95a437ce456d9699f497b0b243092292
luka <lukajiang1998@gmail.com>
2023-04-20 02:10:43 +0000

    half

--- finetune/PaddleNLP/finetune.py
+++ finetune/PaddleNLP/finetune.py
@@ -17,17 +17,6 @@ from dataclasses import dataclass, field
 from functools import partial
 from typing import List, Optional
 
-import mlflow
-
-os.environ["MLFLOW_TRACKING_URI"] = "http://ec2-44-213-176-187.compute-1.amazonaws.com:7003"
-os.environ["MLFLOW_TRACKING_USERNAME"] = "cathayins"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "cathayins"
-simple_experiment = mlflow.set_experiment("lab_luka_test_uie")
-run_tags = {"Name": "lab_luka_test_uie", 
-            "Data": "Simple random sampling", 
-            "XDXDXD": "testest123123"
-}
-mlflow.paddle.autolog()
 import paddle
 from paddlenlp.data import DataCollatorWithPadding
 from paddlenlp.datasets import load_dataset
@@ -94,186 +83,182 @@ def main():
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
     
     
-    with mlflow.start_run(
-        experiment_id=simple_experiment.experiment_id, 
-        run_name="test_mlflow", 
-        tags=run_tags
-    ):
-        
-        
-        if model_args.model_name_or_path in ["uie-m-base", "uie-m-large"]:
-            model_args.multilingual = True
 
-        # Log model and data config
-        training_args.print_config(model_args, "Model")
-        training_args.print_config(data_args, "Data")
-
-        paddle.set_device(training_args.device)
-
-        # Log on each process the small summary:
-        logger.warning(
-            f"Process rank: {training_args.local_rank}, device: {training_args.device}, world_size: {training_args.world_size}, "
-            + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
-        )
-
-        # Detecting last checkpoint.
-        last_checkpoint = None
-        if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
-            last_checkpoint = get_last_checkpoint(training_args.output_dir)
-            if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
-                raise ValueError(
-                    f"Output directory ({training_args.output_dir}) already exists and is not empty. "
-                    "Use --overwrite_output_dir to overcome."
-                )
-            elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
-                logger.info(
-                    f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
-                    "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
-                )
-
-        tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
-        if model_args.multilingual:
-            model = UIEM.from_pretrained(model_args.model_name_or_path)
-        else:
-            model = UIE.from_pretrained(model_args.model_name_or_path)
-
-        train_ds = load_dataset(reader, data_path=data_args.train_path, max_seq_len=data_args.max_seq_length, lazy=False)
-        dev_ds = load_dataset(reader, data_path=data_args.dev_path, max_seq_len=data_args.max_seq_length, lazy=False)
-        # print("test2 ===== ")
-
-        # print(train_ds[2])
-        # print(tokenizer(train_ds[0]["prompt"]))
-        # print(tokenizer(train_ds[0]["content"]))
-        # a = tokenizer(train_ds[0]["content"])
-
-        # print(len(train_ds[3]["content"]))
-        # print(train_ds[0].keys())
-        # print(type(train_ds))
-        trans_fn = partial(
-            convert_example,
-            tokenizer=tokenizer,
-            max_seq_len=data_args.max_seq_length,
-            multilingual=model_args.multilingual,
-            dynamic_max_length=data_args.dynamic_max_length,
-        )
-
-        train_ds = train_ds.map(trans_fn)
-        dev_ds = dev_ds.map(trans_fn)
-
-        if training_args.device == "npu":
-            data_collator = DataCollatorWithPadding(tokenizer, padding="longest")
-        else:
-            data_collator = DataCollatorWithPadding(tokenizer)
-
-        criterion = paddle.nn.BCELoss()
-
-        def uie_loss_func(outputs, labels):
-            start_ids, end_ids = labels
-            start_prob, end_prob = outputs
-            start_ids = paddle.cast(start_ids, "float32")
-            end_ids = paddle.cast(end_ids, "float32")
-            loss_start = criterion(start_prob, start_ids)
-            loss_end = criterion(end_prob, end_ids)
-            loss = (loss_start + loss_end) / 2.0
-            return loss
-
-        def compute_metrics(p):
-            metric = SpanEvaluator()
-            start_prob, end_prob = p.predictions
-            start_ids, end_ids = p.label_ids
-            metric.reset()
-
-            num_correct, num_infer, num_label = metric.compute(start_prob, end_prob, start_ids, end_ids)
-            metric.update(num_correct, num_infer, num_label)
-            precision, recall, f1 = metric.accumulate()
-            metric.reset()
-
-            return {"precision": precision, "recall": recall, "f1": f1}
     
-        trainer = Trainer(
-            model=model,
-            criterion=uie_loss_func,
-            args=training_args,
-            data_collator=data_collator,
-            train_dataset=train_ds if training_args.do_train or training_args.do_compress else None,
-            eval_dataset=dev_ds if training_args.do_eval or training_args.do_compress else None,
-            tokenizer=tokenizer,
-            compute_metrics=compute_metrics,
-        )
+    
+    if model_args.model_name_or_path in ["uie-m-base", "uie-m-large"]:
+        model_args.multilingual = True
 
-        trainer.optimizer = paddle.optimizer.AdamW(learning_rate=training_args.learning_rate, parameters=model.parameters())
-        checkpoint = None
-        if training_args.resume_from_checkpoint is not None:
-            checkpoint = training_args.resume_from_checkpoint
-        elif last_checkpoint is not None:
-            checkpoint = last_checkpoint
+    # Log model and data config
+    training_args.print_config(model_args, "Model")
+    training_args.print_config(data_args, "Data")
 
-        # Training
-        if training_args.do_train:
-            train_result = trainer.train(resume_from_checkpoint=checkpoint)
-            metrics = train_result.metrics
-            trainer.save_model()
-            trainer.log_metrics("train", metrics)
-            trainer.save_metrics("train", metrics)
-            trainer.save_state()
+    paddle.set_device(training_args.device)
 
-        # Evaluate and tests model
-        if training_args.do_eval:
-            eval_metrics = trainer.evaluate()
-            trainer.log_metrics("eval", eval_metrics)
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, world_size: {training_args.world_size}, "
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+    )
 
-        # export inference model
-        if training_args.do_export:
-            # You can also load from certain checkpoint
-            # trainer.load_state_dict_from_checkpoint("/path/to/checkpoint/")
-            if training_args.device == "npu":
-                # npu will transform int64 to int32 for internal calculation.
-                # To reduce useless transformation, we feed int32 inputs.
-                input_spec_dtype = "int32"
-            else:
-                input_spec_dtype = "int64"
-            if model_args.multilingual:
-                input_spec = [
-                    paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="input_ids"),
-                    paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="position_ids"),
-                ]
-            else:
-                input_spec = [
-                    paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="input_ids"),
-                    paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="token_type_ids"),
-                    paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="position_ids"),
-                    paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="attention_mask"),
-                ]
-            if model_args.export_model_dir is None:
-                model_args.export_model_dir = os.path.join(training_args.output_dir, "export")
-            export_model(model=trainer.model, input_spec=input_spec, path=model_args.export_model_dir)
-        if training_args.do_compress:
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
 
-            @paddle.no_grad()
-            def custom_evaluate(self, model, data_loader):
-                metric = SpanEvaluator()
-                model.eval()
-                metric.reset()
-                for batch in data_loader:
-                    if model_args.multilingual:
-                        logits = model(input_ids=batch["input_ids"], position_ids=batch["position_ids"])
-                    else:
-                        logits = model(
-                            input_ids=batch["input_ids"],
-                            token_type_ids=batch["token_type_ids"],
-                            position_ids=batch["position_ids"],
-                            attention_mask=batch["attention_mask"],
-                        )
-                    start_prob, end_prob = logits
-                    start_ids, end_ids = batch["start_positions"], batch["end_positions"]
-                    num_correct, num_infer, num_label = metric.compute(start_prob, end_prob, start_ids, end_ids)
-                    metric.update(num_correct, num_infer, num_label)
-                precision, recall, f1 = metric.accumulate()
-                logger.info("f1: %s, precision: %s, recall: %s" % (f1, precision, f1))
-                model.train()
-                return f1
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
+    if model_args.multilingual:
+        model = UIEM.from_pretrained(model_args.model_name_or_path)
+    else:
+        model = UIE.from_pretrained(model_args.model_name_or_path)
 
-            trainer.compress(custom_evaluate=custom_evaluate)
+    train_ds = load_dataset(reader, data_path=data_args.train_path, max_seq_len=data_args.max_seq_length, lazy=False)
+    dev_ds = load_dataset(reader, data_path=data_args.dev_path, max_seq_len=data_args.max_seq_length, lazy=False)
+    # print("test2 ===== ")
+
+    # print(train_ds[2])
+    # print(tokenizer(train_ds[0]["prompt"]))
+    # print(tokenizer(train_ds[0]["content"]))
+    # a = tokenizer(train_ds[0]["content"])
+
+    # print(len(train_ds[3]["content"]))
+    # print(train_ds[0].keys())
+    # print(type(train_ds))
+    trans_fn = partial(
+        convert_example,
+        tokenizer=tokenizer,
+        max_seq_len=data_args.max_seq_length,
+        multilingual=model_args.multilingual,
+        dynamic_max_length=data_args.dynamic_max_length,
+    )
+
+    train_ds = train_ds.map(trans_fn)
+    dev_ds = dev_ds.map(trans_fn)
+
+    if training_args.device == "npu":
+        data_collator = DataCollatorWithPadding(tokenizer, padding="longest")
+    else:
+        data_collator = DataCollatorWithPadding(tokenizer)
+
+    criterion = paddle.nn.BCELoss()
+
+    def uie_loss_func(outputs, labels):
+        start_ids, end_ids = labels
+        start_prob, end_prob = outputs
+        start_ids = paddle.cast(start_ids, "float32")
+        end_ids = paddle.cast(end_ids, "float32")
+        loss_start = criterion(start_prob, start_ids)
+        loss_end = criterion(end_prob, end_ids)
+        loss = (loss_start + loss_end) / 2.0
+        return loss
+
+    def compute_metrics(p):
+        metric = SpanEvaluator()
+        start_prob, end_prob = p.predictions
+        start_ids, end_ids = p.label_ids
+        metric.reset()
+
+        num_correct, num_infer, num_label = metric.compute(start_prob, end_prob, start_ids, end_ids)
+        metric.update(num_correct, num_infer, num_label)
+        precision, recall, f1 = metric.accumulate()
+        metric.reset()
+
+        return {"precision": precision, "recall": recall, "f1": f1}
+
+    trainer = Trainer(
+        model=model,
+        criterion=uie_loss_func,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_ds if training_args.do_train or training_args.do_compress else None,
+        eval_dataset=dev_ds if training_args.do_eval or training_args.do_compress else None,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.optimizer = paddle.optimizer.AdamW(learning_rate=training_args.learning_rate, parameters=model.parameters())
+    checkpoint = None
+    if training_args.resume_from_checkpoint is not None:
+        checkpoint = training_args.resume_from_checkpoint
+    elif last_checkpoint is not None:
+        checkpoint = last_checkpoint
+
+    # Training
+    if training_args.do_train:
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        metrics = train_result.metrics
+        trainer.save_model()
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+    # Evaluate and tests model
+    if training_args.do_eval:
+        eval_metrics = trainer.evaluate()
+        trainer.log_metrics("eval", eval_metrics)
+
+    # export inference model
+    if training_args.do_export:
+        # You can also load from certain checkpoint
+        # trainer.load_state_dict_from_checkpoint("/path/to/checkpoint/")
+        if training_args.device == "npu":
+            # npu will transform int64 to int32 for internal calculation.
+            # To reduce useless transformation, we feed int32 inputs.
+            input_spec_dtype = "int32"
+        else:
+            input_spec_dtype = "int64"
+        if model_args.multilingual:
+            input_spec = [
+                paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="input_ids"),
+                paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="position_ids"),
+            ]
+        else:
+            input_spec = [
+                paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="input_ids"),
+                paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="token_type_ids"),
+                paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="position_ids"),
+                paddle.static.InputSpec(shape=[None, None], dtype=input_spec_dtype, name="attention_mask"),
+            ]
+        if model_args.export_model_dir is None:
+            model_args.export_model_dir = os.path.join(training_args.output_dir, "export")
+        export_model(model=trainer.model, input_spec=input_spec, path=model_args.export_model_dir)
+    if training_args.do_compress:
+
+        @paddle.no_grad()
+        def custom_evaluate(self, model, data_loader):
+            metric = SpanEvaluator()
+            model.eval()
+            metric.reset()
+            for batch in data_loader:
+                if model_args.multilingual:
+                    logits = model(input_ids=batch["input_ids"], position_ids=batch["position_ids"])
+                else:
+                    logits = model(
+                        input_ids=batch["input_ids"],
+                        token_type_ids=batch["token_type_ids"],
+                        position_ids=batch["position_ids"],
+                        attention_mask=batch["attention_mask"],
+                    )
+                start_prob, end_prob = logits
+                start_ids, end_ids = batch["start_positions"], batch["end_positions"]
+                num_correct, num_infer, num_label = metric.compute(start_prob, end_prob, start_ids, end_ids)
+                metric.update(num_correct, num_infer, num_label)
+            precision, recall, f1 = metric.accumulate()
+            logger.info("f1: %s, precision: %s, recall: %s" % (f1, precision, f1))
+            model.train()
+            return f1
+
+        trainer.compress(custom_evaluate=custom_evaluate)
 
 
 if __name__ == "__main__":
